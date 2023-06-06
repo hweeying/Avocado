6d6138a4fe7da9484a86b8c4eb90be4084b4d507
Jasmine <jasminehweying@gmail.com>
2023-06-06 12:16:37 +0800

    Initial Commit

---
+++ Avocado/AvocadoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadoApp.swift
+//  Avocado
+//
+//  Created by Jasmine Lai Hweeying on 06/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct AvocadoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
